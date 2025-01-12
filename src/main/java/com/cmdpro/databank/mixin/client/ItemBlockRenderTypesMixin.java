d2f49e5caeee406f69fffed4ebb42bb86644cc5c
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2025-01-11 23:58:37 -0500

    mojank why are render types for blocks hardcoded when they exist in block model files

---
+++ src/main/java/com/cmdpro/databank/mixin/client/ItemBlockRenderTypesMixin.java
@@ -0,0 +1,30 @@
+package com.cmdpro.databank.mixin.client;
+
+import com.cmdpro.databank.ClientDatabankUtils;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.BlockModelShaper;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.client.ChunkRenderTypeSet;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+
+@Mixin(ItemBlockRenderTypes.class)
+public class ItemBlockRenderTypesMixin {
+    @Shadow
+    private static Map<Block, RenderType> TYPE_BY_BLOCK;
+    @Inject(method = "getRenderLayers", at = @At(value = "HEAD"), cancellable = true, remap = false)
+    private static void getRenderLayers(BlockState pState, CallbackInfoReturnable<ChunkRenderTypeSet> cir) {
+        BlockState state = ClientDatabankUtils.getHiddenBlock(pState.getBlock());
+        if (state != null) {
+            cir.setReturnValue(ItemBlockRenderTypes.getRenderLayers(state));
+        }
+    }
+}
