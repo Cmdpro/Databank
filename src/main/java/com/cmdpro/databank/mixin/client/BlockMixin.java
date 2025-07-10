fff61f1511950ee283b6e38ee9172bda82793a0a
Gronglegrowth <fyredragonmagus@gmail.com>
2025-07-09 21:49:24 -0400

    blocks too

--- src/main/java/com/cmdpro/databank/mixin/client/BlockMixin.java
+++ src/main/java/com/cmdpro/databank/mixin/client/BlockMixin.java
@@ -4,17 +4,22 @@ import com.cmdpro.databank.hidden.types.BlockHiddenType;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Block.class)
-public class BlockMixin {
+public abstract class BlockMixin {
+    @Shadow protected abstract Block asBlock();
+
     @Inject(method = "getName", at = @At(value = "HEAD"), cancellable = true, remap = false)
     public void getName(CallbackInfoReturnable<MutableComponent> cir) {
         Block block = BlockHiddenType.getHiddenBlockClient((Block)(Object)this);
         if (block != null) {
-            cir.setReturnValue(BlockHiddenType.getHiddenBlockNameOverride(block).orElse(block.getName()).copy());
+            if (block != this.asBlock())
+                cir.setReturnValue(BlockHiddenType.getHiddenBlockNameOverride(block).orElse(block.getName()).copy());
+            cir.setReturnValue(BlockHiddenType.getHiddenBlockNameOverride(block).get().copy());
         }
     }
 }
